3d513fbc0d358df81cae49addcb02b8fe15492ca
kh4idvng <dang.nk.64cntt@ntu.edu.vn>
2025-02-12 16:52:08 +0700

    Ví dụ đầu
    Demo

---
+++ HelloWorld/src/khaidang/XinChao.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package khaidang;
+
+/**
+ * 
+ */
+public class XinChao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Tôi lần đầu học Java và đang thử nghiệm quản lý mã nguồn với Git");
+		
+
+	}
+
+}
