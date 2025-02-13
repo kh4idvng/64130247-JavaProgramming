7756ae1ff9bbd02a574a1416e7262abc409d0a4b
kh4idvng <dang.nk.64cntt@ntu.edu.vn>
2025-02-13 14:29:06 +0700

    Full code. test OK

--- ViDuNhapXuat1/src/ntu/ngokhaidang/CTTinhTong.java
+++ ViDuNhapXuat1/src/ntu/ngokhaidang/CTTinhTong.java
@@ -1,9 +1,26 @@
 package ntu.ngokhaidang;
 
+import java.util.Scanner;
+
 public class CTTinhTong {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// In ra một dòng tiêu đề App
+		System.out.print("CT Tính Tổng \n");
+		
+		// Chuẩn bị cho việc nhập
+		Scanner banPhim = new Scanner(System.in);
+		
+		// In  dòng mời nhập a và b
+		System.out.println("Mời nhập a: ");
+		double a = banPhim.nextDouble();
+		System.out.println("Mời nhập b: ");
+		double b = banPhim.nextDouble();
+		
+		// Tính toán
+		double tong = a + b;
+		System.out.print(tong);
+		
 
 	}
 
