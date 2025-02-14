957ce0365a5b2e515d315c7214d67a00695d49ae
kh4idvng <dang.nk.64cntt@ntu.edu.vn>
2025-02-14 09:25:57 +0700

    da hoan thanh code vi du huong doi tuong

--- ViDuHuongDoiTuong/src/SanPham.java
+++ ViDuHuongDoiTuong/src/SanPham.java
@@ -1,38 +1,62 @@
 
 public class SanPham {
-	
 	//Định nghĩa các thuộc tính
 	int maSP;
 	String tenSP;
 	String loaiSP;
 	String anhSP; // tên file ảnh
-	//Hàm tạo
-	public SanPham() {
-		// khởi tạo giá trị ban đầu cho các thuộc tính nếu có ở đây
-	}
 	
-//	public SanPham(int _maSP, String _tenSP, String _loaiSP, String _anhSP) {
-//		// khởi tạo giá trị ban đầu cho các thuộc tính nếu có ở đây
-//		maSP = _maSP;
-//		tenSP = _tenSP;
-//		loaiSP = _loaiSP;
-//		anhSP = _anhSP;
-//	}
+	public SanPham() {
+		super();
+	}
 
 	public SanPham(int maSP, String tenSP, String loaiSP, String anhSP) {
-		// khởi tạo giá trị ban đầu cho các thuộc tính nếu có ở đây
+		super();
 		this.maSP = maSP;
 		this.tenSP = tenSP;
 		this.loaiSP = loaiSP;
 		this.anhSP = anhSP;
 	}
-	
-	// Các getter, setter
-	public int getMaSP(){
+
+	public int getMaSP() {
 		return maSP;
 	}
+
 	public void setMaSP(int maSP) {
 		this.maSP = maSP;
 	}
+
+	public String getTenSP() {
+		return tenSP;
+	}
+
+	public void setTenSP(String tenSP) {
+		this.tenSP = tenSP;
+	}
+
+	public String getLoaiSP() {
+		return loaiSP;
+	}
+
+	public void setLoaiSP(String loaiSP) {
+		this.loaiSP = loaiSP;
+	}
+
+	public String getAnhSP() {
+		return anhSP;
+	}
+
+	public void setAnhSP(String anhSP) {
+		this.anhSP = anhSP;
+	}
+
+	@Override
+	public String toString() {
+		return " [Mã SP=" + maSP + ", Tên SP=" + tenSP + ", Loại SP=" + loaiSP + "]";
+	}
+	
+	
+	
+	
 
 }
